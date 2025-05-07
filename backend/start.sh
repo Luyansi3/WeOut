ca1f8828d9f71b675579c685144b5999621b61b4
remivCode <remi.vialleton@gmail.com>
2025-05-08 01:10:16 +0200

    removing clash between android studio's port and prisma studio's port

--- backend/start.sh
+++ backend/start.sh
@@ -8,8 +8,9 @@ npm run build
 echo "Migration Prisma..."
 npx prisma migrate deploy
 
-echo "Affichage de la base de données sur le port 5555"
-npx prisma studio &
+echo "Affichage de la base de données sur le port 5554"
+# changing the port to 5554 because 5555 is already used by the android emulator
+npx prisma studio -p 5554 &
 
 echo "Démarrage de l'application..."
 npm run dev
