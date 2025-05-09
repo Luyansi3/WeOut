e7e67ec436e5c0db18368aa161f4500cfda22e10
Luyansi3 <lucas.sanchez@insa-lyon.fr>
2025-05-09 11:47:24 +0200

    Ajout de compilation TS

--- backend/start.sh
+++ backend/start.sh
@@ -12,6 +12,9 @@ echo "Affichage de la base de données sur le port 5553"
 # changing the port to 5553 because 5555 is already used by the android emulator
 npx prisma studio -p 5553 &
 
+echo "Compilation des fichiers TS en watchmode"
+npm run builddev &
+
 echo "Démarrage de l'application..."
 npm run dev
 
