4d9f757f3bb24ef594a38b779372ec42c831e3d6
adrienhdx <ysos4063@gmail.com>
2025-05-09 12:19:02 +0200

    login signup and userprofile work

--- frontend/app/signup.tsx
+++ frontend/app/signup.tsx
@@ -1,23 +1,26 @@
 import { View, Input, Button, Text } from 'tamagui';
 import { useRouter } from 'expo-router';
 
-export default function LoginScreen() {
+export default function SignupScreen() {
     const router = useRouter();
 
     return (
         <View flex={1} justifyContent="center" alignItems="center" gap="$4">
-            <Text fontSize="$6">Login</Text>
+            <Text fontSize="$6">Sign Up</Text>
+            <Input placeholder="Full name" />
             <Input placeholder="Username" />
+            <Input placeholder="Email" />
             <Input placeholder="Password" secureTextEntry />
-            <Button onPress={() => router.push('/home')}>Login</Button>
+            <Input placeholder="Confirm Password" secureTextEntry />
+            <Button onPress={() => router.push('/home')}>Sign Up</Button>
             <Text fontSize="$4">
-                Don't have an account?{' '}
+                Already have an account?{' '}
                 <Text 
                     fontSize="$4" 
                     color="blue" 
-                    onPress={() => router.push('/signup')}
+                    onPress={() => router.push('/login')}
                 >
-                    Sign Up
+                    Login
                 </Text>
             </Text>
         </View>
