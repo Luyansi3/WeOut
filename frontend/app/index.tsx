634d29d992eae007340141d081a1bb1034082200
Riad <148552071+riaddalaoui@users.noreply.github.com>
2025-05-09 15:16:23 +0200

    changement

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,28 +1,20 @@
-// app/index.tsx
-import React from 'react';
 import { StyleSheet } from 'react-native';
-import { View, Button } from 'tamagui';
-import Header from './components/Header';  // ← chemin corrigé
+import { View, Button  } from 'tamagui'
+
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Header title="Accueil" showBackButton={false} />
-
-      <View style={styles.content}>
-        <Button>Welcome!</Button>
-      </View>
+      <Button >Welcome!</Button >
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  content: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
