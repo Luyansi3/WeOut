3a007e78d8bc6b6c0081e60314721a086ceee37f
remivCode <remi.vialleton@gmail.com>
2025-05-10 12:10:30 +0200

    setting up basic map component with react-native-maps

--- frontend/app/(tabs)/map.tsx
+++ frontend/app/(tabs)/map.tsx
@@ -1,12 +1,21 @@
-import { View, Text } from 'tamagui';
-import { useRouter } from 'expo-router';
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
-export default function LoginScreen() {
-    const router = useRouter();
-
-    return (
-        <View flex={1} justifyContent="center" alignItems="center" gap="$4">
-            <Text fontSize="$6">Map</Text>
-        </View>
-    );
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
