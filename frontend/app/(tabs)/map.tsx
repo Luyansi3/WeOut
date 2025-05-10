a7a25276837c5765745aaee2e0dcc1e03b196577
remivCode <remi.vialleton@gmail.com>
2025-05-10 17:57:15 +0200

    modifying config to ts + location + reset location button

--- frontend/app/(tabs)/map.tsx
+++ frontend/app/(tabs)/map.tsx
@@ -1,21 +1,69 @@
-import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useRef, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import * as Location from "expo-location";
+import MapView from "react-native-maps";
+import RecenterButton from "@/components/RecenterMapButton/RecenterMapButton";
 
-export default function App() {
+const Map = () => {
+  const [region, setRegion] = useState(null);
+  const mapRef = useRef(null);
+
+  const setLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      console.log("Permission to access location was denied");
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.High,
+    });
+
+    const newRegion = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.05,
+      longitudeDelta: 0.05,
+    };
+
+    setRegion(newRegion);
+
+    // Animate map view to new region
+    if (mapRef.current) {
+      mapRef.current.animateToRegion(newRegion, 1000); // 1s animation
+    }
+  };
+
+  useEffect(() => {
+    setLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <RecenterButton onPress={setLocation} />
+      {region && (
+        <MapView
+          ref={mapRef}
+          style={styles.map}
+          initialRegion={region}
+          showsUserLocation={true}
+          showsMyLocationButton={false}
+        />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
 });
+
+export default Map;
