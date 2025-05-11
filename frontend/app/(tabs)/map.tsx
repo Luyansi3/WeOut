3d47199961f86c0286d85080c20151dd83ee39f9
remivCode <remi.vialleton@gmail.com>
2025-05-11 11:00:00 +0200

    adding button to put map back to north

--- frontend/app/(tabs)/map.tsx
+++ frontend/app/(tabs)/map.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useRef, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import * as Location from "expo-location";
 import MapView from "react-native-maps";
-import RecenterButton from "@/components/RecenterMapButton/RecenterMapButton";
+import CustomMapButton from "@/components/CustomMapButton/CustomMapButton";
+import { LocateFixed, Compass } from "@tamagui/lucide-icons";
 
 const Map = () => {
   const [region, setRegion] = useState(null);
@@ -34,22 +35,45 @@ const Map = () => {
     }
   };
 
+  const resetToNorth = () => {
+    if (mapRef.current) {
+      mapRef.current.animateCamera({ heading: 0 }, { duration: 1000 });
+    }
+  };
+
   useEffect(() => {
     setLocation();
   }, []);
 
   return (
     <View style={styles.container}>
-      <RecenterButton onPress={setLocation} />
-      {region && (
-        <MapView
-          ref={mapRef}
-          style={styles.map}
-          initialRegion={region}
-          showsUserLocation={true}
-          showsMyLocationButton={false}
+        <CustomMapButton 
+            onPress={setLocation} 
+            Icon={LocateFixed}
+            style={{
+              position: 'absolute',
+              top: 50,
+              right: 20,
+            }}
         />
-      )}
+        <CustomMapButton 
+            onPress={resetToNorth} 
+            Icon={Compass}
+            style={{
+              position: 'absolute',
+              top: 110,
+              right: 20,
+            }}
+        />
+        {region && (
+        <MapView
+            ref={mapRef}
+            style={styles.map}
+            initialRegion={region}
+            showsUserLocation={true}
+            showsMyLocationButton={false}
+        />
+        )}
     </View>
   );
 };
@@ -63,7 +87,7 @@ const styles = StyleSheet.create({
   map: {
     width: "100%",
     height: "100%",
-  },
+  }
 });
 
 export default Map;
