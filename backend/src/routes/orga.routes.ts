f9705cc70d6304ee13258227d8fdf20de7b02458
Selim Ben Bouzid <benbouzid.selim@gmail.com>
2025-05-12 16:59:25 +0200

    route functional

---
+++ backend/src/routes/orga.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import { getOrgaBySoireeId } from "../controllers/orga.controller";
+
+
+const router : Router = Router();
+
+// GET /orgas/getOrgaBySoireeId/:id - récupérer un orga par l'id de la soirée
+
+/**
+ * @openapi
+ * /api/orgas/getOrgaBySoireeId/{id}:
+ *   get:
+ *     tags:
+ *       - Orga
+ *       - Soirees
+ *     summary: recupère un orga par l'id de la soirée qu'il organise
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID unique de la soirée
+ *     responses:
+ *       200:
+ *         description: orga trouvé
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Orga'
+ *       404:
+ *         description: Orga non trouvée
+ *       500:
+ *         description: Erreur serveur
+ */
+router.get('/:id', getOrgaBySoireeId);
+
+export default router;
