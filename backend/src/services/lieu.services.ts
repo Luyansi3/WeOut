1bf29cf3733c1b7c45115a8078f8e97d25492056
Hamza <wiranehamza@gmail.com>
2025-05-13 23:02:26 +0200

    getalllieu

--- backend/src/services/lieu.services.ts
+++ backend/src/services/lieu.services.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Tag } from "@prisma/client";
 import { connect } from "http2";
 import { userInfo } from "os";
 const prisma : PrismaClient = new PrismaClient();
@@ -16,4 +16,37 @@ export const serviceGetLieuById = async(id: number) => {
         }        
 };
 
+export const serviceGetAllLieux = async (tagArray: Tag[], dateFilter?: Date) => {
+    try {
+        return await prisma.lieux.findMany({
+            where: {
+                AND: [
+                    tagArray.length > 0
+                        ? {
+                            tags: {
+                                hasSome: tagArray,
+                            },
+                        }
+                        : {},
 
+                    dateFilter
+                        ? {
+                            soirees: {
+                                some: {
+                                    debut: { lte: dateFilter },
+                                    fin: { gte: dateFilter },
+                                },
+                            },
+                        }
+                        : {},
+                ],
+            },
+            include: {
+                tags: true,
+                soirees: true,
+            },
+        });
+    } catch (error) {
+        throw error;
+    }
+};
